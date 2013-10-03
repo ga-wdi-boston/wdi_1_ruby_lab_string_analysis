68aa73c63636c61346ccdfacc58de9371707251e
Abby Howell <abby.g.howell@gmail.com>
2013-10-02 22:15:38 -0400

    mostly done

--- lib/string_analysis.rb
+++ lib/string_analysis.rb
@@ -12,28 +12,48 @@ def word_frequencies
 	arr = self.split(" ")
 	histogram = Hash.new(0)
 	arr.each do |word|
-		histogram[word.downcase.to_sym] += 1
+		histogram[word.downcase] += 1
 	end
 	return histogram
 end
 
+def unique_words
+	hash = self.word_frequencies
+	arr = []
+	hash.each_key { |key| arr << key }
+	return arr
+end
+
 def unique_word_count
+	self.unique_words.length
+end
 
-	histogram = self.word_frequencies
-	
-	histogram.sort_by |key, value|
-		value
-	end
-	histogram.each do |key, value|
-		delete key if value > 1
-	end
+
+
+# def unique_word_count
+
+# 	histogram = self.word_frequencies
+
+# 	histogram.sort_by |key, value|
+# 		value
+# 	end
+
+# 	histogram.each_key do
+# 		key.delete if value > 1
+# 	end
 		
-end
+# end
 
 
 
 end
 
-str = "in xanadu did kubla khan a stately pleasure dome decree"
+str = "in xanadu did kubla khan a stately pleasure dome decree where alph the sacred river ran through caverns measureless to man down to a sunless sea"
 
-puts str.word_count
+p str.word_count
+
+p str.unique_words
+
+p str.unique_word_count
+
+p str.word_frequencies
