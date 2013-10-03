9602b450a90be0a5dfb41e799cf47b00eae56f51
Abby Howell <abby.g.howell@gmail.com>
2013-10-02 22:23:21 -0400

    done

--- interactive_word_count.rb
+++ interactive_word_count.rb
@@ -2,5 +2,9 @@ require './lib/string_analysis'
 
 puts "Enter a phrase"
 user_phrase = gets
+puts "Total word count: #{user_phrase.word_count}"
 puts "Number of unique words: #{user_phrase.unique_word_count}"
-puts "Word counts: #{user_phrase.word_frequency_count}"
+puts "Word counts: #{user_phrase.word_frequencies}"
+puts "Unique words: #{user_phrase.unique_words}"
+
+
--- lib/string_analysis.rb
+++ lib/string_analysis.rb
@@ -1,14 +1,11 @@
 class String
 
 def word_count
-
 	arr = self.split(" ")
 	return arr.length
-
 end
 
 def word_frequencies
-
 	arr = self.split(" ")
 	histogram = Hash.new(0)
 	arr.each do |word|
@@ -29,31 +26,14 @@ def unique_word_count
 end
 
 
-
-# def unique_word_count
-
-# 	histogram = self.word_frequencies
-
-# 	histogram.sort_by |key, value|
-# 		value
-# 	end
-
-# 	histogram.each_key do
-# 		key.delete if value > 1
-# 	end
-		
-# end
-
-
-
 end
 
-str = "in xanadu did kubla khan a stately pleasure dome decree where alph the sacred river ran through caverns measureless to man down to a sunless sea"
+# str = "in xanadu did kubla khan a stately pleasure dome decree where alph the sacred river ran through caverns measureless to man down to a sunless sea"
 
-p str.word_count
+# p str.word_count
 
-p str.unique_words
+# p str.unique_words
 
-p str.unique_word_count
+# p str.unique_word_count
 
-p str.word_frequencies
+# p str.word_frequencies
