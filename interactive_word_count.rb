11d336ce9bc5e90058d1d792fd761e8637f45dc0
David Fisher <tibbon@gmail.com>
2014-09-30 10:15:24 -0400

    Delete interactive_word_count.rb

--- interactive_word_count.rb
+++
@@ -1,6 +0,0 @@
- require './lib/string_analysis.rb'
-
-puts "Enter a phrase"
-user_phrase = gets
-puts "Number of unique words: #{user_phrase.unique_word_count}"
-puts "Word counts: #{user_phrase.word_frequency_count}"
