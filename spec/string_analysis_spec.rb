29709b7587f70d3882616d8936e0854a428a5f7b
David Fisher <tibbon@gmail.com>
2015-01-12 16:29:59 -0500

    Intentation to spaces

--- spec/string_analysis_spec.rb
+++ spec/string_analysis_spec.rb
@@ -6,11 +6,11 @@ describe String, '#word_count' do
     should respond_to :word_count
   end
 
-	it 'should return 1 when the string is one word long' do
-		a_string = 'apple'
-		the_word_count = a_string.word_count
-		the_word_count.should eq 1
-	end
+  it 'should return 1 when the string is one word long' do
+    a_string = 'apple'
+    the_word_count = a_string.word_count
+    the_word_count.should eq 1
+  end
 
   it 'should return 0 when the string is empty' do
     a_string = ''
