4633672ef28ec6f4e5600e70e282ce506184da5c
Helen Thi <ht5658@gmail.com>
2015-01-14 13:13:08 -0500

    did not get word_frequencies

--- lib/string_analysis.rb
+++ lib/string_analysis.rb
@@ -1,14 +1,23 @@
 # Here we are opening up the String class and adding methods to it
 class String
   def word_count
+    self.split(" ").length
   end
 
   def unique_words
+    array = self.split(" ")
+    array.uniq
   end
 
   def unique_word_count
+    array = self.split(" ")
+    array.uniq
+    array.length
   end
 
   def word_frequencies
+    array = self.split(" ")
+    array.each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1}
   end
 end
+
