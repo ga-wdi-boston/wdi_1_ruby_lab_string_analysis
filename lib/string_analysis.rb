a781db59e3ec1f144f90692bbf81f07ae5a9e272
Anthony Broccoli <acbrocco@syr.edu>
2015-01-14 12:49:35 -0500

    Wrote methods for the assignment

--- lib/string_analysis.rb
+++ lib/string_analysis.rb
@@ -1,14 +1,24 @@
 # Here we are opening up the String class and adding methods to it
 class String
   def word_count
+    self.split.length
   end
 
   def unique_words
+    self.split.uniq
   end
 
   def unique_word_count
+    self.split.uniq.length
   end
 
   def word_frequencies
+    word_count = Hash.new
+    words = self.split
+     words.each do |w|
+      num = words.count{|word| word == w}
+      word_count[w.intern] = num
+    end
+    return word_count
   end
 end
